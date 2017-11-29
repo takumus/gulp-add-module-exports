c0822ee4876c4cccdba65c979303fbe51f58fe72
takumus <me@takumus.com>
2017-11-29 14:32:27 +0900

    gulp用に。

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-/*
-    MIT License http://www.opensource.org/licenses/mit-license.php
-    Author @takumus
-*/
-var ConcatSource = require("webpack-core/lib/ConcatSource");
-function AddModuleExports() {}
-AddModuleExports.prototype.apply = function(compiler) {
-    compiler.plugin("compilation", function(compilation) {
-        compilation.plugin("optimize-chunk-assets", function(chunks, callback) {
-            chunks.forEach(function(chunk) {
-                chunk.files.forEach(function(file) {
-                    compilation.assets[file] = new ConcatSource(
-                        compilation.assets[file].source().replace(
-                            /exports.default/, 'module.exports = exports.default'
-                        ), ''
-                    );
-                });
-            });
-            callback();
-        });
-    });
+var through = require('through2');
+var PluginError = require('gulp-util').PluginError;
+var PLUGIN_NAME = 'add-module-exports';
+var path = require('path');
+module.exports = function(exportName) {
+    var transform = function(file, encoding, callback) {
+        if (file.isNull()) {
+            return callback(null, file);
+        }
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+        }
+        if (file.isBuffer()) {
+            var contents = String(file.contents);
+            var exportsDefault = `exports.default = ${exportName};`;
+            if (contents.indexOf(exportsDefault) >= 0) {
+                console.log(`module.exports added`);
+            }
+            contents = contents.replace(exportsDefault, `${exportsDefault}\nmodule.exports = ${exportName};\nmodule.exports.default = ${exportName}\n`);
+            file.contents = new Buffer(contents);
+            return callback(null, file);
+        }
+        this.push(file);
+        callback();
+    };
+    return through.obj(transform);
 };
-module.exports = AddModuleExports;
