10186260521cce1988d1ece8ae4c8add83998b57
takumus <me@takumus.com>
2017-12-03 03:03:59 +0900

    旧バージョンの引数削除

--- README.md
+++ README.md
@@ -3,12 +3,15 @@
 <https://github.com/takumus/gulp-add-module-exports>
 ## これは何
 module.exportsを追加するgulp用プラグイン。  
-typescriptでライブラリを書き出す時とかに使えるはず。
+typescriptでライブラリを書き出す時とかに使えるはず。  
+`export default`は`export`の前に書いてください。  
+`export`の後に`export default`すると上書きされてしまうので。  
+いつか順番関係なくできるようにしたいです。
 ## インストール
 `npm install gulp-add-module-exports --registry http://npm.takumus.com`
 ## 使い方
     var addModuleExports = require('gulp-add-module-exports');
     ~~
     ~~
-    stream.pipe(addModuleExports("MyLibrary"))
+    stream.pipe(addModuleExports())
     .pipe(gulp.dest("./js"))`
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var through = require('through2');
 var PluginError = require('gulp-util').PluginError;
 var PLUGIN_NAME = 'add-module-exports';
 var path = require('path');
-module.exports = function(exportName) {
+module.exports = function() {
     var transform = function(file, encoding, callback) {
         if (file.isNull()) {
             return callback(null, file);
@@ -12,10 +12,6 @@ module.exports = function(exportName) {
         }
         if (file.isBuffer()) {
             var contents = String(file.contents);
-            //var exportsDefault = `exports.default = ${exportName};`;
-            //if (contents.indexOf(exportsDefault) >= 0) {
-            //    console.log(`module.exports added`);
-            //}
             contents = contents.replace(/exports\.([^\s]*)/g, 'module.exports.$1 = exports.$1');
             contents = contents.replace(/module.exports.default = exports.default = ([^\s;]*);/g, 'module.exports = $1; module.exports.default = $1; exports.default = $1;');
             file.contents = new Buffer(contents);
