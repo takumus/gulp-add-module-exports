270ff56b92e4b34849410afb6f4c83dbaba2f296
takumus <me@takumus.com>
2017-12-04 16:45:10 +0900

    バグ修正

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var through = require('through2');
 var PluginError = require('gulp-util').PluginError;
 var PLUGIN_NAME = 'add-module-exports';
 var path = require('path');
-module.exports = function() {
+module.exports = function(exportName) {
     var transform = function(file, encoding, callback) {
         if (file.isNull()) {
             return callback(null, file);
@@ -12,8 +12,14 @@ module.exports = function() {
         }
         if (file.isBuffer()) {
             var contents = String(file.contents);
-            contents = contents.replace(/exports\.([^\s]*)/g, 'module.exports.$1 = exports.$1');
-            contents = contents.replace(/module.exports.default = exports.default = ([^\s;]*);/g, 'module.exports = $1; module.exports.default = $1; exports.default = $1;');
+            contents = contents.replace(
+                /exports\.([^\s]*) =/g,
+                'module.exports.$1 = exports.$1 ='
+            );
+            contents = contents.replace(
+                /module.exports.default = exports.default = ([^\s;]*);/g,
+                'module.exports = $1; module.exports.default = $1; exports.default = $1;'
+            );
             file.contents = new Buffer(contents);
             return callback(null, file);
         }
